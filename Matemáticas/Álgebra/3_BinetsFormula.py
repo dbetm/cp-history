9e3779e86d19c4a5bafad31e1d88280c047d237e
David <davbetm@gmail.com>
2019-02-01 01:16:09 -0600

    Algoritmo que usa la función de Binet para calcular Fn de la sucesión de Fibonacci

---
+++ Matemáticas/Álgebra/3_BinetsFormula.py
@@ -0,0 +1,11 @@
+# Calcular el n-ésimo elemento de la suceción de Fibonacci
+import math
+def formulaDeBinet(n):
+    res = (((1 + math.sqrt(5)) / 2) ** n) / (math.sqrt(5))
+    return round(res)
+
+n = int(input("Escribe n: "))
+
+fn = formulaDeBinet(n)
+
+print(fn)
