f8e90871c576a6296f8170a276a67670f91f384a
David <davbetm@gmail.com>
2019-03-02 00:46:13 -0600

    Algoritmo de Kadane

--- AlgoritmosComunes/7_Fibonnacci_PD.cpp
+++ AlgoritmosComunes/7_Fibonnacci_PD.cpp
@@ -16,9 +16,9 @@ int obtenerNumeroFibonacci(int n) {
     // Versión iterativa, dinámica, de la sucesión del Fibonacci
     int A[n];
     int ans;
-    A[0] = 1;
+    A[0] = 0;
     A[1] = 1;
     for (int i = 2; i < n; i++) A[i] = A[i - 1] + A[i - 2];
-    ans = A[n - 1];
+    ans = A[n-1];
     return ans;
 }
