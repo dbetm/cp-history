88484e3723b5afeb7e0ebaf8b44d6f3adbc27644
David <davbetm@gmail.com>
2019-06-30 18:37:14 -0500

    Agregar plantillas de C y Java

---
+++ C_template.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// #include <stdlib.h>
+// #include <math.h>
+// 
+typedef unsigned int ui;
+typedef long long int lli;
+typedef unsigned long long int ulli;
+
+int main(void) {
+
+    return 0;
+}
---
+++ Java_template.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+// import java.math.BigInteger;
+//
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next(); // Leer string
+
+        sc.close(); // Cerrar el scanner
+    }
+}
