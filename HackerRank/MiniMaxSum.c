eb254ec82bc5f53c31881be742329f813ced4c70
David Betancourt Montellano <davbetm@gmail.com>
2020-11-20 13:58:42 -0600

    Update MiniMaxSum.c

--- HackerRank/MiniMaxSum.c
+++ HackerRank/MiniMaxSum.c
@@ -12,21 +12,21 @@ int main() {
   double max = 0, min = 0;
   int i, j;
 
-	for(i = 0; i < 5; i++) {
-		scanf("%ld", &A[i]);
-	}
   for(i = 0; i < 5; i++) {
-  	for(j = 0; j < 5; j++) {
-  		if(i == j) continue;
-  		B[i] += A[j];
-  	}
-	}
-	min = B[0];
-	max = B[0];
+    scanf("%ld", &A[i]);
+  }
   for(i = 0; i < 5; i++) {
-		if(B[i] > max) max = B[i];
-		if(B[i] < min) min = B[i];
-	}
+    for(j = 0; j < 5; j++) {
+      if(i == j) continue;
+      B[i] += A[j];
+    }
+  }
+  min = B[0];
+  max = B[0];
+  for(i = 0; i < 5; i++) {
+    if(B[i] > max) max = B[i];
+    if(B[i] < min) min = B[i];
+   }
   
   printf("%.0f %.0f", min, max);
   return 0;
