eb25926b14bb16a282b549c9eafd151ad85d66c3
David <davbetm@gmail.com>
2020-12-06 23:12:45 -0600

    HR: Resolver Array manipulation

--- HackerRank/Estructuras de datos/4_ArrayManipulation.cpp
+++ HackerRank/Estructuras de datos/4_ArrayManipulation.cpp
@@ -1,37 +1,33 @@
 #include <bits/stdc++.h>
-//
-// Tag(s):
+// https://www.hackerrank.com/challenges/crush/problem
+// Tag(s): Lazy propagation, arrays
 using namespace std;
 #define watch(x) cout << (#x) << " es " << (x) << endl;
-#define EPS 1.19209e-07
 #define fastIO ios_base::sync_with_stdio(NULL); cin.tie(NULL); cout.tie(NULL);
-typedef long long int lli;
-typedef vector<int> vi;
-typedef vector<long long int> vlli;
-typedef vector<double> vd;
-lli MAX = 1e9+7;
+
+long int arr[10000000];
 
 int main() {
     fastIO
     int n, m, a, b;
     cin >> n >> m;
-    lli arr[n+1] = {0}, k, aux;
-    uint64_t hip = 0, delta;
 
+    int64_t k;
+    int64_t aux, hip;
     while(m--) {
         cin >> a >> b >> k;
+        a -= 1;
+        b -= 1;
         arr[a] += k;
-        if(b+1 <= n) arr[b+1] -= k;
+        if((b+1) < n) arr[b+1] -= k;
     }
 
-    delta = arr[1];
-    hip = arr[1];
-    for (int i = 2; i <= n; i++) {
-        aux = arr[i];
-        arr[i] += delta;
-        delta = aux;
-        if(arr[i] > hip) {
-            hip = arr[i];
+    aux = arr[0];
+    hip = arr[0];
+    for (int i = 1; i <= n; i++) {
+        aux += arr[i];
+        if(aux > hip) {
+            hip = aux;
         }
     }
 
