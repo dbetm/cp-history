025abbbddcddd815541f9345287218f3b0726622
dbetm <davbetm@gmail.com>
2022-10-30 18:00:40 -0600

    add HR problem - string construction

--- HackerRank/Strings/14_StringConstruction.py
+++ HackerRank/Strings/14_StringConstruction.py
@@ -1,20 +1,24 @@
 #
 # Tag(s):
 
+"""
 def compute_cost(s):
     n = len(s)
     cost = 0
 
     all_subs = set()
-    p = ''
+    p = ""
 
     for i in range(n):
         x = s[i]
+        k = i
+
         if x in all_subs:
+            # hacer append de todas las subcadenas que se pueden usar sin costo
             while True:
-                if (i + 1) < n and (x + s[i + 1]) in all_subs:
-                    i += 1
-                    x += s[i]
+                if (k + 1) < n and (x + s[k + 1]) in all_subs:
+                    k += 1
+                    x += s[k]
                 else:
                     break
         else:
@@ -24,13 +28,50 @@ def compute_cost(s):
             all_subs.add(x)
 
         p += x
-        # print("p", p, "x", x)
+
+        if k >= n:
+            break
+
         m = len(p)
         for j in range(m-1, -1, -1):
             if not p[j:] in all_subs:
                 all_subs.add(p[j:])
 
     return cost
+"""
+
+"""
+def compute_cost(s: str) -> int:
+    n = len(s)
+
+    p = s[0]
+    cost = 1
+
+    for i in range(1, n):
+        has_cost = True
+        for j in range(n, i, -1):
+            if s[i:j] in p:
+                p += s[i:j]
+                i = j
+                has_cost = False
+                break
+
+        if has_cost:
+            p += s[i]
+            cost += 1
+
+    return cost
+"""
+
+
+"""Solution:
+It doesn't matter if the resting substring in s is large or not, there's no cost, so
+the problem is reduced to count the unique characters, because always we can use a single
+char to copy from s to p.
+"""
+
+def compute_cost(s: str) -> int:
+    return len(set(s))
 
 
 if __name__ == '__main__':
