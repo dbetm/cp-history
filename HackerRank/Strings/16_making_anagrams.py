5a0002197089d892e5ab77013d7fbb31ca9dbefc
dbetm <davbetm@gmail.com>
2023-02-06 00:25:45 -0600

    HackerRank - making anagrams solved

---
+++ HackerRank/Strings/16_making_anagrams.py
@@ -0,0 +1,37 @@
+# https://www.hackerrank.com/challenges/making-anagrams/problem
+# tag(s): strings, implementation
+
+def create_map(s: str) -> dict:
+    map = dict()
+
+    for c in s:
+        if c not in map:
+            map[c] = 1
+        else:
+            map[c] += 1
+
+    return map
+
+
+def get_min_deletions(s1_map: dict, s2_map: dict) -> int:
+    total_deletions = 0
+
+    for s1_key, value in s1_map.items():
+        if s1_key not in s2_map:
+            total_deletions += value
+        else:
+            total_deletions += abs(value - s2_map[s1_key])
+            del s2_map[s1_key]
+    
+    for _, value in s2_map.items():
+        total_deletions += value
+
+    return total_deletions
+
+
+
+if __name__ == "__main__":
+    s1 = input()
+    s2 = input()
+
+    print(get_min_deletions(create_map(s1), create_map(s2)))
