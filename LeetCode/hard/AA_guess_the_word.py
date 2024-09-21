ebf1c357923be0646873233eb010a0a67b968422
David Betancourt <davbetm@gmail.com>
2024-09-21 17:22:17 -0600

    LeetCode solve Guess the word

--- LeetCode/hard/AA_guess_the_word.py
+++ LeetCode/hard/AA_guess_the_word.py
@@ -1,9 +1,7 @@
-#
-# tags: 
-
+# https://leetcode.com/problems/guess-the-word
+# tags: divide-and-conquer, implementation, strings
 
 import random
-from collections import defaultdict
 from typing import List
 
 
@@ -44,68 +42,33 @@ class Master:
 
 class Solution:
     def findSecretWord(self, words: List[str], master: Master) -> None:
-        matching_table = [defaultdict(lambda : set()) for _ in range(MAX_LENGHT_WORD)]
+        """Proposal
+        Time: O(n^2)
+        Space: O(n)
+        """
+
+        # 1) Iterate until found the secret word or check all the words (n guesses)
+        # 2) At each iteration, reduce the number of set of words to check
+        # 3) To reduce the wordlist, only consider the words that have the same match of chars
+        # that the choosen word had with the secret word
+
         n = len(words)
 
-        for i in range(n):
-            ref_word = words[i]
+        for _ in range(n):
+            candidate = random.choice(words)
 
-            for j in range(i+1, n):
-                matches = count_matches(ref_word, words[j])
-                matching_table[matches][i].add(j)
-                matching_table[matches][j].add(i)
+            matches = master.guess(candidate)
 
-        from pprint import pprint
-        pprint(matching_table)
+            if matches == MAX_LENGHT_WORD:
+                break
 
-        checked_words = set()
+            candidates = list()
 
-        for i in range(MAX_LENGHT_WORD - 1, -1, -1):
-            for ref_idx, assoc_jdx in matching_table[i].items():
-                if words[ref_idx] in checked_words:
-                    continue
-                
-                matches = master.guess(words[ref_idx])
+            for word in words:
+                if count_matches(candidate, word) == matches:
+                    candidates.append(word)
 
-                print(f"{words[ref_idx]=}")
-
-                if matches == MAX_LENGHT_WORD:
-                    return
-
-                if matches == 0 and i != 0:
-                    continue
-
-                checked_words.add(words[ref_idx])
-
-                for jdx in assoc_jdx:
-                    if words[ref_idx] in checked_words:
-                        continue
-
-                    matches = master.guess(words[jdx])
-
-                    if matches == MAX_LENGHT_WORD:
-                        return
-
-                    checked_words.add(words[jdx])
-
-        # for i in range(n):
-        #     if words[i] in checked_words:
-        #         continue
-        #     matches = master.guess(words[i])
-        #     checked_words.add(words[i])
-
-        #     if matches == MAX_LENGHT_WORD:
-        #         break
-
-        #     # look at matches 
-        #     for jdx_word_to_check in matching_table[matches][i]:
-        #         if words[jdx_word_to_check] in checked_words:
-        #             continue
-
-        #         if master.guess(words[jdx_word_to_check]) == MAX_LENGHT_WORD:
-        #             return
-
-        #         checked_words.add(words[jdx_word_to_check])
+            words = candidates
 
 
 
