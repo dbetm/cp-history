9e0b2083cb287e209a5af225ce4bc082de773643
David Betancourt <davbetm@gmail.com>
2024-10-01 12:03:26 -0600

    AE solve Knight Connection

--- AlgoExpert/hard/007_knight_connection.py
+++ AlgoExpert/hard/007_knight_connection.py
@@ -1,65 +1,45 @@
+import math
 from typing import Union
 
 
-def compute_distance(coord_a: tuple, coord_b: tuple) -> Union[float, int]:
-    if coord_a[0] < 0 or coord_a[1] < 0 or coord_b[0] < 0 or coord_b[1] < 0:
-        return float("inf")
 
-    return abs(coord_a[0] - coord_b[0]) + abs(coord_a[1] - coord_b[1])
+def is_the_same_position(a: Union[list, tuple], b: Union[list, tuple]) -> bool:
+    NUM_DIMS = 2
+    assert len(a) >= NUM_DIMS
+    assert len(b) >= NUM_DIMS
 
+    return a[0] == b[0] and a[1] == b[1]
 
-MOVEMENTS = (
-    (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)
-)
-
-
-def has_capured(coord_a: tuple, coord_b: tuple):
-    return coord_a[0] == coord_b[0] and coord_a[1] == coord_b[1]
-
-
-
-def update_coord(current_coord, coord_to_cmp):
-    distance = float("inf")
-    new_coord_a = None
-
-    for deltas in MOVEMENTS:
-        coord = (current_coord[0] + deltas[0], current_coord[1] + deltas[1])
-
-        tmp_distance = compute_distance(coord, coord_to_cmp)
-
-        if tmp_distance < distance:
-            distance = tmp_distance
-            new_coord_a = coord
-
-    return new_coord_a
-
-
-import time
 
 def knightConnection(knightA, knightB):
-    turns = 0
-    coord_a = (knightA[0], knightA[1])
-    coord_b = (knightB[0], knightB[1])
+    """Proposal
+    Time: O(n * m)
+    Space: O(n * m)
 
-    if has_capured(coord_a, coord_b):
-        return turns
+    Where n is the horizontal distance between the knights and m the vertical one.
+    """
+    deltas = [
+        (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)
+    ]
+    # coordinate of knight and the distance in rounds to the another knight
+    position = (knightA[0], knightA[1], 0)
+    visited_positions = {(position[0], position[1])}
+    next_movements = [position]
 
     while True:
-        turns += 1
-        print(f"{coord_a=}", f"{coord_b=}")
-        coord_a = update_coord(coord_a, coord_b)
+        position = next_movements.pop(0)
 
-        if has_capured(coord_a, coord_b):
-            break
+        if is_the_same_position(position, knightB):
+            return math.ceil(position[2] / 2)
 
-        coord_b = update_coord(coord_b, coord_a)
+        for delta in deltas:
+            new_pos = (position[0] + delta[0], position[1] + delta[1])
 
-        if has_capured(coord_a, coord_b):
-            break
+            if new_pos in visited_positions:
+                continue
 
-        time.sleep(2)
-
-    return turns
+            visited_positions.add(new_pos)
+            next_movements.append((new_pos[0], new_pos[1], position[2] + 1))
 
 
 
